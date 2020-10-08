c7657a1542214b28e0e8c611cf18a6aebaa6e9cb
Anthony D <CypharX@gmail.com>
2020-10-07 20:08:30 -0400

    Initial commit

---
+++ Scripts/Custom.js
@@ -0,0 +1,26 @@
+﻿document.getElementById('fizzBtn').addEventListener('click', function () {
+    let fizz = parseInt(document.getElementById('input1').value);
+    let buzz = parseInt(document.getElementById('input2').value);
+    let fizzedOut = fizzThis(fizz, buzz);
+    document.getElementById('output').innerHTML = fizzedOut;
+});
+function fizzThis(fizz, buzz) {
+    let output = "";
+    for (let loop = 1; loop <= 100; loop++) {        
+            let fizzCheck = loop % fizz;
+            let buzzCheck = loop % buzz;
+            if (fizzCheck == 0 && buzzCheck == 0) {
+                output += 'FIZZBUZZ!, ';
+            }
+            else if (fizzCheck == 0) {
+                output += 'Fizz, ';
+            }
+            else if (buzzCheck == 0) {
+                output += 'Buzz, ';
+            }
+            else {
+                output += `${loop}, `;
+            }
+        }
+    return output
+    }
